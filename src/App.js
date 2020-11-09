e188720bf86a6604dcee7471bb600c74d8fc3957
Leslie Rodriguez <rleslie1015@gmail.com>
2020-11-09 09:24:19 -0600

    adds layout and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Pages from './pages';
 
+// styles
+import GlobalStyle from './components/GlobalStyle';
+
 // routes
 import Routes from './pages';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Pages />
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-// components
+// shared layout
+import Layout from '../components/Layout';
+// routes
 import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
@@ -9,9 +11,11 @@ import Favorites from './favorites';
 const Pages = () => {
     return (
         <Router>
-            <Route exact path='/' component={Home} />
-            <Route path='/mynotes' component={MyNotes} />
-            <Route path='/favorites' component={Favorites} />
+            <Layout>
+                <Route exact path='/' component={Home} />
+                <Route path='/mynotes' component={MyNotes} />
+                <Route path='/favorites' component={Favorites} />
+            </Layout>
         </Router>
     )
 }
